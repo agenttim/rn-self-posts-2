97e4f2e9d36a76d2119ac5f9df3327c64ac03c6d
agent_tim <agent_tim@mail.ru>
2023-10-30 10:30:43 +0500

    внедрил createMaterialBottomTabNavigator

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {Platform} from 'react-native';
+import {Platform, SafeAreaView} from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
 
 import {MainScreen, mainScreenOptions} from '../screens/MainScreen';
@@ -11,44 +11,55 @@ import {BookedScreen, bookedScreenOptions} from '../screens/BookedScreen';
 
 import {THEME} from '../theme';
 import {Post} from "../components/Post";
+import {createMaterialBottomTabNavigator} from "react-native-paper/react-navigation";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
 const PostStack = createStackNavigator();
 
 const BookedStack = createStackNavigator();
 
-const Tab = createBottomTabNavigator();
+const Tab = Platform.OS === 'android' ? createMaterialBottomTabNavigator() : createBottomTabNavigator();
 
 export default function AppNavigation() {
 
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={() => ({
-                    tabBarActiveTintColor: THEME.MAIN_COLOR,
-                    headerShown: false
-                })}
-            >
-                <Tab.Screen
-                    name="Post"
-                    component={PostNavigator}
-                    options={{
-                        tabBarIcon: route => (
-                            <Ionicons name='ios-albums' size={25} color={route.color}/>
-                        )
-                    }}
-                />
-                <Tab.Screen
-                    name="Booked"
-                    component={BookedNavigator}
-                    options={{
-                        tabBarIcon: route => (
-                            <Ionicons name='ios-star' size={25} color={route.color}/>
-                        )
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={() => ({
+                        tabBarActiveTintColor: THEME.MAIN_COLOR,
+                        headerShown: false
+                    })}
+                    activeColor="#fff"
+                    inactiveColor={THEME.INACTIVE_COLOR}
+                    barStyle={{ backgroundColor: THEME.MAIN_COLOR }}
+                    shifting={true}
+                >
+                    <Tab.Screen
+                        name="Post"
+                        component={PostNavigator}
+                        options={{
+                            tabBarIcon: route => (
+                                <Ionicons name='ios-albums' size={25} color={route.color}/>
+                            ),
+                            tabBarLabel: 'Все'
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Booked"
+                        component={BookedNavigator}
+                        options={{
+                            tabBarIcon: route => (
+                                <Ionicons name='ios-star' size={25} color={route.color}/>
+                            ),
+                            tabBarLabel: 'Избранное'
+                        }}
+                    />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+
+    )
 
 }
 
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,5 @@
 export const THEME = {
     MAIN_COLOR: '#303f9f',
+    INACTIVE_COLOR: '#a1a8d8',
     DANGER_COLOR: '#d81b60'
 }
