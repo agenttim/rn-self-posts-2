e9fef08647046db69cf37412aed7d45764c9aa29
agent_tim <agent_tim@mail.ru>
2023-11-01 08:35:31 +0500

    реализовал создание поста

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,18 +1,66 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Image,
+    Button,
+    ScrollView,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
 import {HeaderButtons} from "react-navigation-header-buttons/src/HeaderButtons";
 import {AppHeaderIcon} from "../components/AppHeaderIcon";
 import {Item} from "react-navigation-header-buttons";
+import {THEME} from "../theme";
+import {useDispatch} from "react-redux";
+import {addPost} from "../store/actions/post";
 
 export const CreateScreen = ({navigation}) => {
+
+    const [text, setText] = useState('')
+    const dispatch = useDispatch()
+    const  img = 'https://sun9-70.userapi.com/sun9-44/impg/niGS92BiCE6urW0aWHRl8lAvfioDsduQ6ef7-g/iCee4wWYTHo.jpg?size=1280x853&quality=96&sign=5ef4219a4aa80189de480a78b8a2d11b&c_uniq_tag=ngcM3dN1_-Xy8e5KM468ZpTPOqAyXYzBy_7oUxSWBSc&type=album'
+
+    const saveHandler = () => {
+        const post = {
+            date: new Date().toJSON(),
+            text: text,
+            img: img,
+            booked: false
+        }
+        dispatch(addPost(post))
+        navigation.navigate('Main')
+    }
+
     return (
-        <View style={styles.center}>
-            <Text>CreateScreen</Text>
-        </View>
+        <ScrollView>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss}>
+                <View style={styles.wrapper}>
+                    <Text style={styles.title}>Создай новый пост</Text>
+                    <TextInput
+                        style={styles.textarea}
+                        placeholder="Введите текст заметки"
+                        value={text}
+                        onChangeText={setText}
+                        multiline
+                    />
+                    <Image
+                        style={{width: '100%', height: 200, marginBottom: 10}}
+                        source={{uri: img}}/>
+                    <Button title='Создать пост' color={THEME.MAIN_COLOR} onPress={saveHandler}/>
+                </View>
+            </TouchableWithoutFeedback>
+        </ScrollView>
     )
 }
 
-export function createScreenOptions({navigation}) {
+export function createScreenOptions(
+    {
+        navigation
+    }
+) {
     return {
         headerTitle: 'Создать пост',
         headerLeft: () => (
@@ -27,10 +75,21 @@ export function createScreenOptions({navigation}) {
     }
 }
 
-const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+const styles = StyleSheet.create(
+    {
+        wrapper: {
+            padding: 10
+        }
+        ,
+        title: {
+            fontSize: 20,
+            textAlign: "center",
+            fontFamily: "open-regular",
+            marginVertical: 10
+        },
+        textarea: {
+            padding: 10,
+            marginBottom: 10
+        }
     }
-})
+)
